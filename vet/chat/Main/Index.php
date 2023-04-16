837db3983736905b96bdf4105ec114dced6d6299
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2023-04-17 01:13:26 +0530

    chat2

--- vet/chat/Main/Index.php
+++ vet/chat/Main/Index.php
@@ -11,9 +11,6 @@
     </style>
     <link rel="stylesheet" href="chatindex.css">
 
-    <!-- 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
 </head>
 
@@ -21,7 +18,7 @@
 
 
     <div class="container">
-        <h1>WebSocket with ChatRoom</h1>
+        <h1>Free Consultation</h1>
         <?php
       
         session_start();
@@ -35,7 +32,7 @@
         }
         if (isset($_SESSION['userid'])) {
             echo "Logged in as " . $_SESSION['userid'];
-            echo " <a href='Logout.php'>Logout</a>";
+            echo " <a href='../../vetDashboard.php'>Logout</a>";
         } else {
           
         }
