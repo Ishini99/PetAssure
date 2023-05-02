0256d1a0edca965a57e34270af7bd8b63d7d368b
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2023-05-02 22:36:42 +0530

    new home page

---
+++ home.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>PetAssure</title>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    
+</head>
+ 
+<body>
+    <header>
+        <div class="wrapper">
+            <h1>PetAssure</h1>
+            <nav>
+                <ul>
+                   
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="sigupserviceprovider.php">SignUp as service provider</a></li>
+                    <li><a href="signup_client.php">SignUp as client</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    <div id="image">
+        <div class="wrapper">
+          
+          
+        </div>
+    </div>
+    
+  
+    
+    <div id="primary-content">
+        <div class="wrapper">
+            <article>
+                <h2>Local pet care services for your best friend.</h2>
+                <hr>
+                <h3>
+
+                    Welcome to our pet care center, where we provide exceptional care for cats and dogs. Our trained staff is dedicated to ensuring that your furry friends receive the attention and love they deserve. We offer a range of services, including pet sitting, boarding, medical administration, grooming, and more. Our facilities are designed with your pet's comfort and safety in mind, so you can have peace of mind knowing they're in good hands. At our pet care center, we treat your pets like family. </h3>
+               
+            </article>
+        </div>
+    </div>
+    <div id="features">
+        <div class="wrapper1">
+            <ul>
+                <li class="feature">
+                    <h3>Safety</h3>
+                    <p>Each of our playrooms is staffed by our team of trained attendants and all areas are cleaned throughout the day.</p>
+                </li>
+                <li class="feature">
+                    <h3>All-in-one Pricing</h3>
+                    <p>Treat your pet to all-inclusive fun at convenient rate.</p>
+                </li>
+                <li class="feature">
+                    <h3>24/7 Care</h3>
+                    <p>Pets daycare staff are on premises 24 hours a day, 365 days a year to provide round-the-clock loving care. </p>
+                </li>
+                <div class="clear"></div>
+            </ul>
+        </div>
+    </div>
+    <div id="primary-content">
+        <div class="wrapper">
+            <article>
+                <h2>Our Services </h2>
+                <hr>
+               
+               
+            </article>
+        </div>
+    </div>
+    <div id="secondary-content">
+        <div class="wrapper">
+            <article style="background-image: url('images/img4.jpg');">
+                <div class="overlay">
+                    <h3>Pets </h3>
+                    <h5><small>At our petcare center, we provide exceptional care for your furry friends, whether they are cats or dogs. Our trained staff will ensure they receive the attention and love they deserve while in our care..</small></h5>
+                   
+                </div>
+            </article>
+            <article style="background-image: url('images/a19cb97468ba341e56effe25d463d91c.jpg');">
+                <div class="overlay">
+                    <h3>Pet Sitting</h3>
+                    <h5><small>While you’re away we can make sure your pet has all the food, water and, of course, attention, they deserve.Helping your new pup to be a good boy or girl isn’t always easy, but we’re here to help.</small></h5>
+                   
+                </div>
+            </article>
+            <article style="background-image: url('images/aa976b5c77de3379b5dd94e6e01597b2.jpg');">
+                <div class="overlay">
+                    <h3>Pet Boarding </h3>
+                    <h5><small>Pet boarding in our pet care center provides a comfortable and safe environment for your cat or dog while you're away. Our experienced staff ensures that your pet receives proper care and attention during their stay.</small></h5>
+                   
+                </div>
+            </article>
+            <article style="background-image: url('images/IMG-20230501-WA0044.jpg');">
+                <div class="overlay">
+                    <h3>Pet Medical Administration</h3>
+                    <h5><small>Our team of experienced professionals can help with everything from pills to injections.</small></h5>
+                   
+                </div>
+            </article>
+            <article style="background-image: url('images/IMG-20230501-WA0046.jpg');">
+                <div class="overlay">
+                    <h3>Pet Grooming</h3>
+                    <h5><small>Treat your furry friend to a spa day at our Pet Grooming center, where they'll be pampered with love and care. Our expert groomers will have your pet looking and feeling their best.</small></h5>
+                   
+                </div>
+            </article>
+            <article style="background-image: url('images/img5.jpg');">
+                <div class="overlay">
+                    <h3>Pet Friendly Places</h3>
+                    <h5><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt nam.</small></h5>
+                   
+                </div>
+            </article><div class="clear"></div>
+        </div>
+    </div>
+   
+    <div class="footerr" style="position:absolute; z-index: -1; width: 100%;">
+        <p> Telephone No: +94 11 233 5632
+            Fax: +94 11 233 5632
+            Email: petAssure@hotmail.com​</p>
+    </div>
+    
+</body>
+</html>
